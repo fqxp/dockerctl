7c429f200a88e0880830d192407bfee69440473b
Frank Ploss <frank@fqxp.de>
2013-12-06 12:46:08 +0100

    fix broken debian package

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ import os
 version_w = open('VERSION').read().strip()
 version = '0.1'
 
-print 'TRUE' if version_w==version else 'FALEA'
+open('dockerctl/version.py', 'w').write('version = %s' % version)
 
 setup(name='dockerctl',
       version=version,
       author='Frank Ploss',
       author_email='frank@fqxp.de',
-      py_modules=['dockerctl'],
-      scripts=['dockerctl'],
+      packages=['dockerctl'],
+      scripts=['bin/dockerctl'],
       description='A tool for managing docker containers using per-container config files',
       data_files=[
           ('/usr/share/doc/dockerctl', ['README.md']),
