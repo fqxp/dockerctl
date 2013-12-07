6a03c22648583bf628c60b6dccc29731b8ac94d5
Frank Ploss <frank@fqxp.de>
2013-12-07 16:32:12 +0100

    move datetime functions into dockerctl.utils

--- dockerctl/container.py
+++ dockerctl/container.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python2.7
 
+from dockerctl.utils import pretty_date, parse_datetime
 import datetime
 import json
 import yaml
@@ -8,53 +9,6 @@ import os.path
 import re
 import subprocess
 import sys
-
-
-def pretty_date(time=False):
-    """
-    Get a datetime object or a int() Epoch timestamp and return a
-    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
-    'just now', etc
-    """
-    from datetime import datetime
-    now = datetime.utcnow()
-    if type(time) is int:
-        diff = now - datetime.fromtimestamp(time)
-    elif isinstance(time,datetime):
-        diff = now - time
-    elif not time:
-        diff = now - now
-    second_diff = diff.seconds
-    day_diff = diff.days
-
-    if day_diff < 0:
-        return ''
-
-    if day_diff == 0:
-        if second_diff < 10:
-            return "just now"
-        if second_diff < 60:
-            return str(second_diff) + " seconds ago"
-        if second_diff < 120:
-            return  "a minute ago"
-        if second_diff < 3600:
-            return str( second_diff / 60 ) + " minutes ago"
-        if second_diff < 7200:
-            return "an hour ago"
-        if second_diff < 86400:
-            return str( second_diff / 3600 ) + " hours ago"
-    if day_diff == 1:
-        return "Yesterday"
-    if day_diff < 7:
-        return str(day_diff) + " days ago"
-    if day_diff < 31:
-        return str(day_diff/7) + " weeks ago"
-    if day_diff < 365:
-        return str(day_diff/30) + " months ago"
-    return str(day_diff/365) + " years ago"
-
-def parse_datetime(s):
-    return datetime.datetime(*map(int, re.split('[^\d]', s)[:-2]))
 
 
 class ContainerException(Exception):
